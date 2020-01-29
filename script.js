c0981717cd333d73987f6eab81bc2f5818c5bb4e
Pablo Garcia <garciapablo838@gmail.com>
2020-01-29 15:48:57 -0300

    probando

--- script.js
+++ script.js
@@ -18,3 +18,92 @@ readJson()
     console.error("no se encuentra el archivo json");
     console.log(e);
   });
+
+  
+
+
+  async function borzoi() {
+    try {
+      var response = await fetch("https://dog.ceo/api/breed/borzoi/images/random");
+      var data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  var cuentaBorzoi;
+  borzoi()
+    .then(function(data) {
+      cuentaBorzoi = data;
+      console.log(cuentaBorzoi);
+    })
+    .catch(function(e) {
+      console.error("no se encuentra el archivo json");
+      console.log(e);
+    });
+
+
+    async function cairn() {
+      try {
+        var response = await fetch("https://dog.ceo/api/breed/cairn/images/random");
+        var data = await response.json();
+        return data;
+      } catch (error) {
+        throw error;
+      }
+    }
+    
+    var cuentaCairn;
+    cairn()
+      .then(function(data) {
+        cuentaCairn = data;
+        console.log(cuentaCairn);
+      })
+      .catch(function(e) {
+        console.error("no se encuentra el archivo json");
+        console.log(e);
+      });
+
+
+      async function shiba() {
+        try {
+          var response = await fetch("https://dog.ceo/api/breed/shiba/images/random");
+          var data = await response.json();
+          return data;
+        } catch (error) {
+          throw error;
+        }
+      }
+      
+      var cuentaShiba;
+      shiba()
+        .then(function(data) {
+          cuentaShiba = data;
+          console.log(cuentaShiba);
+        })
+        .catch(function(e) {
+          console.error("no se encuentra el archivo json");
+          console.log(e);
+        });
+
+        async function vizsla() {
+          try {
+            var response = await fetch("https://dog.ceo/api/breed/vizsla/images/random");
+            var data = await response.json();
+            return data;
+          } catch (error) {
+            throw error;
+          }
+        }
+        
+        var cuentaVizsla;
+        vizsla()
+          .then(function(data) {
+            cuentaVizsla = data;
+            console.log(cuentaVizsla);
+          })
+          .catch(function(e) {
+            console.error("no se encuentra el archivo json");
+            console.log(e);
+          });
