9804924c210c87aa0fd0b2bce5edad19121856fd
NicolasMitre <nicolasmelluso7@gmail.com>
2020-01-30 12:52:32 -0300

    top ten functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,8 @@
 // DECLARACION DE VARIABLES.
 
-
-var queridos = [""];
-var noQueridos = [""];
-
-
+var queridos = [];
+var noQueridos = [];
+var acumulador;
 // FIN DECLARACION DE VARIABLES.
 
 async function start() {
@@ -26,23 +24,33 @@ start()
     console.log(e);
   });
 
+async function coleccionPerro() {
+  try {
+    var response = await fetch("https://dog.ceo/api/breeds/image/random");
+    var data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function mostrarFoto(url) {
+  try {
+    var response = await fetch(
+      "https://dog.ceo/api/breed/" + url.message + "/images/random"
+    );
+    var data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
 function pasarUrl(url) {
   console.log(url.message);
   mostrarTitulo(url.message);
 
-  async function mostrarFoto() {
-    try {
-      var response = await fetch(
-        "https://dog.ceo/api/breed/" + url.message + "/images/random"
-      );
-      var data = await response.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  mostrarFoto()
+  mostrarFoto(url)
     .then(function(data) {
       document.getElementById("main").src = data.message;
     })
@@ -56,16 +64,35 @@ function mostrarTitulo(mensaje) {
   document.getElementById("titulo").textContent = mensaje;
 }
 
-
-
 function votarSi() {
-  queridos.push(document.getElementById("perrito").textContent);
+  queridos.push(document.getElementById("titulo").textContent);
+  ver();
 }
-
 
 function votarNo() {
-  noQueridos.push(document.getElementById("perrito").textContent);
+  noQueridos.push(document.getElementById("titulo").textContent);
+  ver();
 }
 
-console.log("queridos:", queridos);
-console.log("no queridos", noQueridos);
+function ver() {
+  if (acumulador == 5) {
+    coleccionPerro()
+      .then(function(data) {
+        const div = document.getElementById("suggestions");
+        const image = document.createElement("img");
+        image.setAttribute("src", data.message);
+        div.appendChild(image);
+      })
+      .catch(function(e) {
+        console.error("no se encuentra el archivo json");
+        console.log(e);
+      });
+    acumulador = 0;
+    queridos = [];
+    noQueridos = [];
+    console.log(acumulador);
+  } else {
+    acumulador = queridos.length + noQueridos.length;
+    console.log(acumulador);
+  }
+}
